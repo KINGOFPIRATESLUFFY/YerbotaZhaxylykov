56cab7bb6638615e75d8552dce7482d3ca51d8d1
YerbotaZhaxylykov <98817472+KINGOFPIRATESLUFFY@users.noreply.github.com>
2024-09-10 15:18:08 +0600

    Lab1

---
+++ Datatypes.py
@@ -0,0 +1,32 @@
+x = "Hello World"
+x = 20
+x = 20.5
+x = 1j
+x = ["apple", "banana", "cherry"]
+x = ("apple", "banana", "cherry")
+x = range(6)
+x = {"name" : "John", "age" : 36}		
+x = {"apple", "banana", "cherry"}	
+x = frozenset({"apple", "banana", "cherry"})	
+x = True		
+x = b"Hello"		
+x = bytearray(5)	
+x = memoryview(bytes(5))	
+x = None
+
+
+#Specific data type
+x = str("Hello World")       	              #str	
+x = int(20)	                                  #int	
+x = float(20.5)                               #float	
+x = complex(1j)	                              #complex	
+x = list(("apple", "banana", "cherry"))	      #list	
+x = tuple(("apple", "banana", "cherry"))	  #tuple	
+x = range(6)	                              #range	
+x = dict(name="John", age=36)	              #dict	
+x = set(("apple", "banana", "cherry"))	      #set	
+x = frozenset(("apple", "banana", "cherry"))  #frozenset	
+x = bool(5)	                                  #bool	
+x = bytes(5)	                              #bytes	
+x = bytearray(5)	                          #bytearray	
+x = memoryview(bytes(5))
