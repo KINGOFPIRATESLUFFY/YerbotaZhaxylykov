883f2fbcfc6f37d857a8067b59c3c96357b4d0a5
YerbotaZhaxylykov <98817472+KINGOFPIRATESLUFFY@users.noreply.github.com>
2024-09-10 12:59:27 +0600

    Lab1

---
+++ Comments.py
@@ -0,0 +1,14 @@
+#This is a comment
+print("Hello World!")
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, world!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello World!")
